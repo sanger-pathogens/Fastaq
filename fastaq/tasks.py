c45a495e368b39034febb9b5bc47c2d3b7b1bfa9
martinghunt <martin.g.hunt@gmail.com>
2015-01-09 13:25:35 +0000

    Add task mean_length

--- fastaq/tasks.py
+++ fastaq/tasks.py
@@ -423,6 +423,21 @@ def make_long_reads(infile, outfile, method='tiling', fixed_read_length=20000, t
     utils.close(f)
 
 
+def mean_length(infile, limit=None):
+    '''Returns the mean length of the sequences in the input file. By default uses all sequences. To limit to the first N sequences, use limit=N'''
+    total = 0
+    count = 0
+    seq_reader = sequences.file_reader(infile)
+    for seq in seq_reader:
+        total += len(seq)
+        count += 1
+        if limit is not None and count >= limit:
+            break
+
+    assert count > 0
+    return total / count
+
+
 def merge_to_one_seq(infile, outfile, seqname='union'):
     '''Takes a multi fasta or fastq file and writes a new file that contains just one sequence, with the original sequences catted together, preserving their order'''
     seq_reader = sequences.file_reader(infile)
