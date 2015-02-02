30435ce73361bca7bb38437171083565ef3311dd
Martin Hunt <mh12@sanger.ac.uk>
2015-02-02 11:19:24 +0000

    Changes for pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 import os
 import glob
+import sys
 from setuptools import setup, find_packages
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+try:
+    import numpy
+except ImportError:
+    print("Error! numpy for Python3 not found.\nPlease install it (e.g. apt-get install python3-numpy)", file=sys.stderr)
+    sys.exit(1)
 
 setup(
     name='pyfastaq',
     version='3.0.0',
-    description='Scripts to manipulate FASTA and FASTQ files, plus API for developers',
-    long_description=read('README.md'),
+    description='Script to manipulate FASTA and FASTQ files, plus API for developers',
     packages = find_packages(),
     author='Martin Hunt',
     author_email='mh12@sanger.ac.uk',
@@ -18,4 +22,10 @@ setup(
     test_suite='nose.collector',
     install_requires=['nose >= 1.3'],
     license='GPLv3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
 )
