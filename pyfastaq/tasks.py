7d0dde46fa10e9ac7e22e28f96c2567b05de6972
martinghunt <martin.g.hunt@gmail.com>
2015-04-20 09:49:21 +0100

    make headers in the style of SPAdes

--- pyfastaq/tasks.py
+++ pyfastaq/tasks.py
@@ -598,7 +598,7 @@ def sort_by_size(infile, outfile, smallest_first=False):
 
 
 def to_fastg(infile, outfile, circular=None):
-    '''Writes a FASTG file from input file. Currently only whether or not a sequence is circular is supported. Put circular=set of ids, or circular=filename to make those sequences circular in the output'''
+    '''Writes a FASTG file in SPAdes format from input file. Currently only whether or not a sequence is circular is supported. Put circular=set of ids, or circular=filename to make those sequences circular in the output. Puts coverage=1 on all contigs'''
     if circular is None:
         to_circularise = set()
     elif type(circular) is not set:
@@ -610,18 +610,30 @@ def to_fastg(infile, outfile, circular=None):
 
     seq_reader = sequences.file_reader(infile)
     fout = utils.open_file_write(outfile)
+    nodes = 1
 
     for seq in seq_reader:
+        new_id = '_'.join([
+            'NODE', str(nodes),
+            'length', str(len(seq)),
+            'cov', '1',
+            'ID', seq.id
+        ])
+
         if seq.id in to_circularise:
-            original_id = seq.id
-            seq.id = original_id + ':' + original_id + ';'
+            seq.id = new_id + ':' + new_id + ';'
             print(seq, file=fout)
             seq.revcomp()
-            seq.id = original_id + "':" + original_id + "';"
+            seq.id = new_id + "':" + new_id + "';"
             print(seq, file=fout)
         else:
-            seq.id += ';'
+            seq.id = new_id + ';'
             print(seq, file=fout)
+            seq.revcomp()
+            seq.id = new_id + "';"
+            print(seq, file=fout)
+
+        nodes += 1
 
     utils.close(fout)
 
