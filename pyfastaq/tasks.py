e06064ed189abecb06e728a88901ef437b05eba2
martinghunt <martin.g.hunt@gmail.com>
2015-10-13 15:52:27 +0100

    new task length_offsets_from_fai

--- pyfastaq/tasks.py
+++ pyfastaq/tasks.py
@@ -680,6 +680,29 @@ def lengths_from_fai(fai_file, d):
     utils.close(f)
 
 
+def length_offsets_from_fai(fai_file):
+    '''Returns a dictionary of positions of the start of each sequence, as
+       if all the sequences were catted into one sequence.
+       eg if file has three sequences, seq1 10bp, seq2 30bp, seq3 20bp, then
+       the output would be: {'seq1': 0, 'seq2': 10, 'seq3': 40}'''
+    positions = {}
+    total_length = 0
+    f = utils.open_file_read(fai_file)
+
+    for line in f:
+        try:
+            (name, length) = line.rstrip().split()[:2]
+            length = int(length)
+        except:
+            raise Error('Error reading the following line of fai file ' + fai_file + '\n' + line)
+
+        positions[name] = total_length
+        total_length += length
+
+    utils.close(f)
+    return positions
+
+
 def split_by_base_count(infile, outfiles_prefix, max_bases, max_seqs=None):
     '''Splits a fasta/q file into separate files, file size determined by number of bases.
 
