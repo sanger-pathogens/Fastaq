55ba3a7f0a147bd78364a49a5bd8046be2b1f434
Étienne Mollier <emollier@debian.org>
2024-10-16 11:10:39 +0200

    Accomodate properly for local tests.
    When packaging information is missing, the initial proposal missed
    falling back to the 'local' version properly.  This change adjusts the
    try/except logic to accomodate to that situation properly.
    
    Signed-off-by: Étienne Mollier <emollier@debian.org>

--- pyfastaq/__init__.py
+++ pyfastaq/__init__.py
@@ -1,11 +1,12 @@
 try:
     from importlib.metadata import Distribution
     __version__ = Distribution().from_name('pyfastaq').version
-except ImportError:
-    from pkg_resources import get_distribution
-    __version__ = get_distribution('pyfastaq').version
 except:
-    __version__ = 'local'
+    try:
+        from pkg_resources import get_distribution
+        __version__ = get_distribution('pyfastaq').version
+    except:
+        __version__ = 'local'
 
 
 
